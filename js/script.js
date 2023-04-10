10d6815c00ada56b7e46c7d1252bf3768332adad
hv1225 <r0937950@student.thomasmore.be>
2023-04-10 02:54:20 +0200

    minor html changes and start scripting

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-//blablabla
+//elements
+const voornaam = document.getElementById('voornaam');
+const naam = document.getElementById('naam');
+const gebruikersnaam = document.getElementById('gebruikersnaam');
+const email = document.getElementById('email');
+const wachtwoord = document.getElementById('wachtwoord');
+const wachtwoordHerhaal = document.getElementById('wachtwoordHerhaal');
