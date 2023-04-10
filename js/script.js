3e7c757f056740e6667ae2499e0fb6cbb0d203e3
hv1225 <r0937950@student.thomasmore.be>
2023-04-10 11:42:02 +0200

    scripting progress, html id adjustments

--- js/script.js
+++ js/script.js
@@ -5,5 +5,27 @@ const gebruikersnaam = document.getElementById('gebruikersnaam');
 const email = document.getElementById('email');
 const wachtwoord = document.getElementById('wachtwoord');
 const wachtwoordHerhaal = document.getElementById('wachtwoordHerhaal');
+const land = document.getElementById('land');
+const provincie = document.getElementById('provincie');
+const postcode = document.getElementById('postcode');
+const nieuwsbrief = document.getElementById('nieuwsbrief');
+const voorwaarden = document.getElementById('voorwaarden');
+const bankapp = document.getElementById('bankapp');
+const overschrijving = document.getElementById('overschrijving');
+const visa = document.getElementById('visa');
+const paypal = document.getElementById('paypal');
 
 //other elements
+const form = document.getElementById('form');
+const error = document.getElementById('error');
+const succes = document.getElementById('succes');
+const betalingswijze = document.getElementById('betalingswijze');
+
+//events
+form.addEventListener('submit', (e) =>
+{
+
+
+})
+
+//methods
