b765097b2f22bcf8d9fd8d2b495a0cc2054ba833
hv1225 <r0937950@student.thomasmore.be>
2023-04-10 02:58:05 +0200

    start scripting 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-//elements
+//form elements
 const voornaam = document.getElementById('voornaam');
 const naam = document.getElementById('naam');
 const gebruikersnaam = document.getElementById('gebruikersnaam');
 const email = document.getElementById('email');
 const wachtwoord = document.getElementById('wachtwoord');
 const wachtwoordHerhaal = document.getElementById('wachtwoordHerhaal');
+
+//other elements
